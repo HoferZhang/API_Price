444f34156f30fe964b9e32a2cdf59f71f339355b
Hofer.Zhanng <mrhongfeng@163.com>
2017-11-30 02:53:25 +0800

    perfect

--- test1.py
+++ test1.py
@@ -1,26 +1,82 @@
+# -*- coding: utf8 -*-
+
 import requests
 import json
+import xlrd
+import xlwt
 
 
-url_DocIndex = "http://test.anxinyisheng.com/home/user/doc_detail"
+CaseFileName = "case.xlsx"
+SheetName = "Sheet1"
 
-headers = {
-    "Wxid":"o79aixECshqXft8Cck5fMC7LdYZs",
-    "Channel": "wx_anxinjiankang",
-    "User-Agent": "micromessenger"}
-paramDocIndex = {
-    "_id": "59c4fe257f1008c12c8b4579",
+url_DocIndex = \
+    "http://test.anxinyisheng.com/home/user/doc_detail"
+
+paramDoc = {
     "bizid": "573ae98d7f10087a048b4a61",
     "from": "doctor_list"}
 
-rspDocIndex = requests.get(url=url_DocIndex, params=paramDocIndex, headers=headers)
+headers = {
+    "Wxid": "o79aixECshqXft8Cck5fMC7LdYZs",
+    "Channel": "wx_anxinjiankang",
+    "User-Agent": "micromessenger"}
 
-rspJson = json.loads(rspDocIndex.text)
-docID = rspJson["msg"]["_id"]
-docName = rspJson["msg"]["name"]
-consultPrice = rspJson["msg"]["priceList"]["consultationPrice"]
-qaPrice = rspJson["msg"]["priceList"]["qaPrice"]
 
-print (rspDocIndex.status_code)
-print (rspDocIndex.text)
-print (rspDocIndex.text)
+# 执行请求，保存结果
+def run():
+    totalcase = get_sum_case(CaseFileName, SheetName)
+    lenresult = len(get_result(url_DocIndex, get_param(paramDoc, CaseFileName, 0), headers))
+    matrix = [[0 for i in range(lenresult)] for i in range(totalcase)]
+
+    for i in range(totalcase):
+        matrix[i] = get_result(url_DocIndex, get_param(paramDoc, CaseFileName, i), headers)
+
+    print (matrix)
+
+    w = xlwt.Workbook()
+    ws = w.add_sheet(u'Sheet1')
+
+    for m in range(len(matrix)):
+        for n in range(len(matrix[0])):
+            ws.write(m, n, matrix[m][n])
+
+    w.save('resultw.xls')
+
+
+# 请求接口，获取结果
+def get_result(url, params, header):
+    rsp = requests.get(url=url, params=params, headers=header)
+    rsp_json = json.loads(rsp.text)
+
+    doc_id = rsp_json["msg"]["_id"]
+    doc_name = rsp_json["msg"]["name"]
+    hospital_name = rsp_json["msg"]["hospital"]["name"]
+    hospital_level = rsp_json["msg"]["hospital"]["level"]
+    consult_price = rsp_json["msg"]["priceList"]["consultationPrice"]
+    qa_price = rsp_json["msg"]["priceList"]["qaPrice"]
+
+    result = [doc_id, doc_name, hospital_name, hospital_level, consult_price, qa_price]
+
+    return result
+
+
+def get_param(param, filename, row):
+    case = xlrd.open_workbook(filename)
+    sheet = case.sheet_by_name("Sheet1")
+
+    # 获取行数据
+    while row < sheet.nrows:
+        param["_id"] = sheet.cell_value(row, 0)
+        return param
+    else:
+        return 0
+
+
+def get_sum_case(filename, sheetname):
+    workbook = xlrd.open_workbook(filename)
+    sheet = workbook.sheet_by_name(sheetname)
+
+    return sheet.nrows
+
+
+run()
