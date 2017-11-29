a09490261b70132e7c6d1664eab31022afb4581c
Hofer <zhang.hofer@gmail.com>
2017-11-29 20:32:31 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,26 @@
-a = 1
+import requests
+import json
+
+
+url_DocIndex = "http://test.anxinyisheng.com/home/user/doc_detail"
+
+headers = {
+    "Wxid":"o79aixECshqXft8Cck5fMC7LdYZs",
+    "Channel": "wx_anxinjiankang",
+    "User-Agent": "micromessenger"}
+paramDocIndex = {
+    "_id": "59c4fe257f1008c12c8b4579",
+    "bizid": "573ae98d7f10087a048b4a61",
+    "from": "doctor_list"}
+
+rspDocIndex = requests.get(url=url_DocIndex, params=paramDocIndex, headers=headers)
+
+rspJson = json.loads(rspDocIndex.text)
+docID = rspJson["msg"]["_id"]
+docName = rspJson["msg"]["name"]
+consultPrice = rspJson["msg"]["priceList"]["consultationPrice"]
+qaPrice = rspJson["msg"]["priceList"]["qaPrice"]
+
+print (rspDocIndex.status_code)
+print (rspDocIndex.text)
+print (rspDocIndex.text)
