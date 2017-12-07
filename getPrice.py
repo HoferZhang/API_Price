6c8f08cafc3d08f6ff85b565bd14a48708427bb0
HoferZhang <Zhang.hofer@gmail.com>
2017-12-07 18:10:52 +0800

    log id

--- getPrice.py
+++ getPrice.py
@@ -6,46 +6,44 @@ import xlrd
 import xlwt
 import time
 
-
 CurrentTime = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
 CaseFileName = "case.xlsx"
-ResultFileName = "Result_"+CurrentTime+".xls"
+ResultFileName = "Result_" + CurrentTime + ".xls"
 SheetName = "Sheet1"
 
 paramDoc = {
     "bizid": "573ae98d7f10087a048b4a61",
     "from": "doctor_list"}
 
-# 测试环境
-url_DocIndex = \
-    "http://test.anxinyisheng.com/home/user/doc_detail"
-
-headers = {
-    "Wxid": "o79aixECshqXft8Cck5fMC7LdYZs",
-    "Channel": "wx_anxinjiankang",
-    "User-Agent": "micromessenger"}
-
-# # 线上环境
+# # 测试环境
 # url_DocIndex = \
-#     "http://mobile.anxinyisheng.com/home/user/doc_detail"
+#     "http://test.anxinyisheng.com/home/user/doc_detail"
 #
 # headers = {
-#     "Wxid": "oKoQnuEYYwxn7QbqKFIMiQJS-G_s",
+#     "Wxid": "o79aixECshqXft8Cck5fMC7LdYZs",
 #     "Channel": "wx_anxinjiankang",
 #     "User-Agent": "micromessenger"}
 
+# 线上环境
+url_DocIndex = \
+    "http://mobile.anxinyisheng.com/home/user/doc_detail"
+
+headers = {
+    "Wxid": "oKoQnuEYYwxn7QbqKFIMiQJS-G_s",
+    "Channel": "wx_anxinjiankang",
+    "User-Agent": "micromessenger"}
 
 
 # 记录请求结果，并保存
 def run(name):
     print ("\n*****************************\n测试开始，正在获取用例总数...")
     totalcase = get_sum_case(CaseFileName, SheetName)
-    print ("  用例总数为：%s\n    开始执行..."%totalcase)
+    print ("  用例总数为：%s\n    开始执行..." % (totalcase + 1))
     lenresult = len(get_result(url_DocIndex, get_param(paramDoc, CaseFileName, 0), headers))
     matrix = [[0 for i in range(lenresult)] for i in range(totalcase)]
 
     for i in range(totalcase):
-        print ("      正在执行caseId：%s"%i)
+        print ("      正在执行caseId：%s" % (i + 1))
         matrix[i] = get_result(url_DocIndex, get_param(paramDoc, CaseFileName, i), headers)
 
     # print (matrix)
@@ -59,7 +57,7 @@ def run(name):
 
     for m in range(len(matrix)):
         for n in range(len(matrix[0])):
-            ws.write(m+1, n, matrix[m][n])
+            ws.write(m + 1, n, matrix[m][n])
 
     w.save(name)
     print ("  保存成功，执行结束\n*****************************")
