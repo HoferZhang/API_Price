b2b6f541daf51dd6e34f2bb69c37fb7ee02cce6b
Hofer <zhang.hofer@gmail.com>
2018-09-20 18:22:21 +0800

    support python3

--- ChatPriceSelf.py
+++ ChatPriceSelf.py
@@ -27,35 +27,38 @@ TEST_WX_HEADRER = {
 
 
 def run_entrance():
-    host = select_env()[0]
-    wx_header = select_env()[1]
+    info = select_env()
+    host = info[0]
+    wx_header = info[1]
 
-    print ("\n*****************************\n测试开始，正在获取用例总数...")
+    print("\n*****************************\n测试开始，正在获取用例总数...")
     totalcase = get_sum_case(CaseFileName, 'Sheet1')
-    print ("  用例总数为：%s\n    开始执行..." % totalcase)
+    print("  用例总数为：%s\n    开始执行..." % totalcase)
 
     lenresult = len(chat_price(host, wx_header, read_case(CaseFileName, 1)[0], read_case(CaseFileName, 1)[1]))
     matrix = [[0 for i in range(lenresult)] for i in range(totalcase)]
+    # print(matrix)
 
-    for i in range(totalcase-1):
-        print ("      正在执行caseId：%s" % (i + 1))
-        matrix[i] = chat_price(host, wx_header, read_case(CaseFileName, i+1)[0], read_case(CaseFileName, i+1)[1] )
+    for i in range(totalcase):
+        print("      正在执行caseId：%s" % (i + 1))
+        matrix[i] = chat_price(host, wx_header, read_case(CaseFileName, i)[0], read_case(CaseFileName, i)[1])
+    print(matrix)
 
-    # print (matrix)
-
-    print ("    执行完毕，开始保存结果...")
+    print("    执行完毕，开始保存结果...")
     w = xlwt.Workbook()
     ws = w.add_sheet(u'Sheet1')
     raw0 = ["mobile", "docid", "docName", "hospital", "priceList", "currentPrice", "setPrice", "wxPrice"]
     for i in range(len(raw0)):
         ws.write(0, i, raw0[i])
 
+    print(len(matrix))
+    print(len(matrix[0]))
     for m in range(len(matrix)):
         for n in range(len(matrix[0])):
             ws.write(m + 1, n, matrix[m][n])
 
     w.save(ResultFileName)
-    print ("  保存成功，执行结束\n*****************************")
+    print("  保存成功，执行结束\n*****************************")
 
 
 def chat_price(host, wx_header, mobile, price, pwd=123456):
@@ -76,25 +79,32 @@ def chat_price(host, wx_header, mobile, price, pwd=123456):
     # 获取医生信息
     url_me = host + '/home/doctor/me'
     rsp_me = requests.post(url=url_me, headers=header).json()
-    info = [rsp_me["mobile"], rsp_me["_id"], rsp_me["name"], rsp_me["hospital"]["name"]]
+    # print(rsp_me)
+    info = [rsp_me["msg"]["mobile"], rsp_me["msg"]["_id"], rsp_me["msg"]["name"], rsp_me["msg"]["hospital"]["name"]]
 
     # 获取医生可配置收入列表
-    url_price_list = host + 'xxxxxxxxx'  # 填写获取医生价格列表地址
+    url_price_list = host + '/home/chat/getDocChatPriceOptions'  # 获取医生价格列表地址
     rsp_price_list = requests.post(url=url_price_list, headers=header).json()
-    price_list = [rsp_price_list["msg"]["priceList"], rsp_price_list["msg"]["currentPrice"], price]
+    a_list = rsp_price_list["msg"]["priceList"]
+    s_list = ''
+    for i in range(len(a_list)-1):
+        s_list = str(a_list[i]) + ',' + str(a_list[i+1])
+    price_list = [s_list, str(rsp_price_list["msg"]["currentPrice"]), str(price)]
 
     # 医生提交自主定价
-    url_set_price = host + 'xxxxxxxxx'  # 填写提交医生价格地址
+    url_set_price = host + '/home/chat/docSetChatPriceConf'  # 填写提交医生价格地址
     data = dict(price=price)
-    requests.post(url=url_set_price, dada=data, headers=header).json()
+    requests.post(url=url_set_price, data=data, headers=header).json()
 
     # 患者端获取医生在线咨询服务价格
     url_wx_doc_main = host + '/home/user/doc_detail'
-    params =dict(_id=rsp_me["_id"], bizid=rsp_me["_id"])
+    params =dict(_id=rsp_me["msg"]["_id"], bizid=rsp_me["msg"]["_id"])
     rsp_wx_doc_main = requests.get(url=url_wx_doc_main, params=params, headers=wx_header).json()
-    price_wx = [rsp_wx_doc_main["msg"]["priceList"]["consultationPrice"]]
+    price_wx = [str(rsp_wx_doc_main["msg"]["priceList"]["consultationPrice"])]
 
-    return info + price_list + price_wx
+    result = info + price_list + price_wx
+    # print(result)
+    return result
 
 
 # 选择执行环境
@@ -108,12 +118,11 @@ def select_env():
     if host == 1:  # 线上正式环境
         print('\n已选择正式环境，即将开始执行...')
         return [ONLINE_HOST, ONLINE_WX_HEADER]
-
     elif host == 2:  # 测试环境
         print('\n已选择测试环境，即将开始执行...')
         return [TEST_HOST, TEST_WX_HEADRER]
-    else:  # 输入不合法
-        print('\n输入错误，请重新输入\n')
+    else:
+        print('\n输入错误，请重新输入...')
         select_env()
 
 
